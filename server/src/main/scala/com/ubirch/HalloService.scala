ecc3414a520ea5185a044e82d6070f2b00ccfe13
Matthias L. Jugel <matthias.jugel@streamdrill.com>
2015-10-12 18:07:21 +0200

    added multi-message and IMEI coupling

--- server/src/main/scala/com/ubirch/HalloService.scala
+++ server/src/main/scala/com/ubirch/HalloService.scala
@@ -1,18 +1,35 @@
 package com.ubirch
 
-import java.io.{File, FileOutputStream}
+import java.io.{FilenameFilter, File, FileOutputStream}
+import java.text.SimpleDateFormat
+import java.util.{TimeZone, Calendar}
 
 import akka.actor.Actor
-import spray.http._
+import spray.http.HttpResponse
 import spray.routing._
+
+import scala.util.Try
+
+case class CleanupFile(file: Option[File])
 
 class HalloServiceActor extends Actor with HalloService {
   def actorRefFactory = context
 
-  def receive = runRoute(routes)
+  def receive = runRoute(routes) orElse {
+    case CleanupFile(file) => file.foreach(f => Try(f.delete))
+  }
+
 }
 
 trait HalloService extends HttpService {
+  val dataDir = new File("data")
+  var connections: Map[String, String] = Map(
+    //    "866224021719199" -> "866224021719199"
+  )
+
+  if (!dataDir.exists()) {
+    dataDir.mkdirs()
+  }
 
   val routes =
     path("") {
@@ -20,19 +37,47 @@ trait HalloService extends HttpService {
         complete("HALLO")
       }
     } ~
-      path("""\d+""".r) { id =>
+      path( """\d+""".r) { id =>
         detach() {
           get {
-            getFromFile(s"upload_$id.ogg")
-          } ~
-          post {
-            entity(as[Array[Byte]]) { value =>
-              val byteWriter = new FileOutputStream(new File(s"upload_$id.ogg"))
-              byteWriter.write(value)
-              byteWriter.flush()
-              byteWriter.close()
-              println(s"RECEIVED FILE: $id")
+            if (!dataDir.exists()) reject()
+
+            // try to find the newest file available
+            val file = dataDir.listFiles
+              .filter(f => f.getName.matches(s"message_${id}_\\d+\\.ogg"))
+              .sortBy(_.getName).headOption
+
+            // delete file after successful delivery
+            mapRequestContext { ctx =>
+              ctx.withRouteResponseMapped {
+                case r: HttpResponse => r.withAck(CleanupFile(file))
+                case x => x
+              }
+            } {
+              file match {
+                case Some(f) => getFromFile(f)
+                case None => reject
+              }
+            }
+          }
+        } ~ post {
+          entity(as[Array[Byte]]) { value =>
+            val recipient = connections.getOrElse(id, id)
+            val cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+            val df = new SimpleDateFormat("yyyyMMddHHmmss")
+            val name = s"message_${recipient}_${df.format(cal.getTimeInMillis)}"
+            val file = new File(dataDir, s"$name.tmp")
+            val byteWriter = new FileOutputStream(file)
+            byteWriter.write(value)
+            byteWriter.flush()
+            byteWriter.close()
+
+            println(s"RECEIVED FILE: $id")
+            val target = new File(dataDir, s"$name.ogg")
+            if(file.renameTo(target)) {
               complete(s"$id")
+            } else {
+              reject
             }
           }
         }
